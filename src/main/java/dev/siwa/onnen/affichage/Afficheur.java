bb958f90d57c2c5c1ccb6d4f4997af6d026b64d3
Siwa12100 <jean.marcillac12@gmail.com>
2023-07-13 20:11:57 +0200

    commentaires et mise au propre du code

--- src/main/java/dev/siwa/onnen/affichage/Afficheur.java
+++ src/main/java/dev/siwa/onnen/affichage/Afficheur.java
@@ -11,44 +11,58 @@ public class Afficheur implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
 
+        // On commence par vérifier si ce qui entre la commande est bien un joueur
         if (sender instanceof Player) {
+            // Si c'est le cas, on transforme celui qui a envoyé la commande en joueur
             Player p = (Player) sender;
+            // Variable utilisée pour savoir combien de fois envoyer le message dans la commande /crier
             int nbRepetitions = 1;
+            // Variable pour si broadcast ou chuchoter le message dans la commande /crier
             boolean broadcast = true;
+            // Permet de concaténer les différents Strings dans la commande /crier
             StringBuilder bt = new StringBuilder();
 
+            // On regarde si la commande est le /helloworld
             if (cmd.getName().equalsIgnoreCase("helloworld")) {
-                System.out.println("Passage dans Hellword....(temp)");
+                // Si c'est bien la commande, on envoie un petit message au joueur
                 p.sendMessage("Salut " + p.getName() + " !");
 
                 return true;
             }
 
-            // Ne fonctionne pas encore tout à fait mais on y est presque...
+            // On regarde si la commmande est le /crier
             if (cmd.getName().equalsIgnoreCase("crier")) {
-                System.out.println("Passage dans Crier....(temp)");
-
+                // On parcours le tableau d'arguments passé à la commande
                 for (int i = 0; i < args.length; i++) {
+                    // On regarde quel est l'argument actuel
                     switch (args[i]) {
+                        // Option qui permet d'indiquer le nombre de répétitions du message
                         case "-r":
+                            // On vérifie qu'il y a bien quelque chose après le -r
                             if (i+1== args.length) {
                                 p.sendMessage("[Erreur] : Il manque un paramètre à l'option -r !");
                                 return false;
                             }
+                            // Si il y a bien quelque chose, on met à jour la variable nbRepetitions
                             nbRepetitions = Integer.valueOf(args[i+1]);
+                            // On passe à l'argument suivant
                             i++;
                             break;
-
+                        // Option qui permet de spécifier si on envoie en broadcast ou non. Par défaut, c'est du broadcast
                         case "-c":
                             broadcast = false;
                             break;
-
+                        // Option (obligatoire) qui permet de rentrer le message à envoyer
                         case "-m":
+                            // Tant que l'on arrive pas à la fin du tableau, on parcours les arguments
                             while(i+1 < args.length){
+                                // Si on a l'option -r ou -c, il ne s'agit plus du message à envoyer alors on quitte le while
                                 if (args[i+1].equals("-r") || args[i+1].equals("-c")){
                                     break;
                                 }
+                                // On ajoute l'argument actuel au message à envoyer
                                 bt.append(args[i+1] + " ");
+                                // On passe à l'argument suivant
                                 i++;
                             }
                             break;
@@ -58,14 +72,15 @@ public class Afficheur implements CommandExecutor {
                             return false;
                     }
                 }
-
+                // On transforme le message à envoyer en un String unique
                 String texte = bt.toString();
-
+                // On regarde si c'est du broadcast
                 if (broadcast == true) {
+                    // On envoie le message selon le nombre de répétitions spécifiées
                     for (int e = 0; e < nbRepetitions; e++) {
-                        Bukkit.broadcastMessage(p.getName() + "crie " + (e+1) + "fois : \"" + texte + "\"");
+                        Bukkit.broadcastMessage(p.getName() + " crie " + (e+1) + " fois : \"" + texte + "\"");
                     }
-                } else {
+                } else { // Si c'est pas du broadcast, on chuchote
                     for (int e = 0; e < nbRepetitions; e++) {
                         p.sendMessage("Vous chuchotez " + (e+1) + " fois : \"" + texte + "\"");
                     }
@@ -73,7 +88,6 @@ public class Afficheur implements CommandExecutor {
                 return true;
             }
         }
-
         return false;
     }
 }
